dac38973c02358476fa1f35fb878c291446ce8d9
Shaun Mhonde <mhondeshaun@gmail.com>
2019-08-28 20:21:13 +0200

    feat: locking an image to a fixed aspect ratio

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="bg-white border rounded-lg overflow-hidden">
-		<!-- For IE11 use background image -->
-		<!-- <div class="h-48 bg-cover bg-center" :style="{ backgroundImage: `url('${property.imageUrl}')` }"></div> -->
-		<img class="h-48 w-full object-cover" :src="property.imageUrl" :alt="property.imageAlt">
+		<div class="relative bg-red-500 pb-2/3">
+			<!-- For IE11 use background image -->
+			<!-- <div class="absolute h-full w-full bg-cover bg-center" :style="{ backgroundImage: `url('${property.imageUrl}')` }"></div> -->
+			<img class="absolute h-full w-full object-cover" :src="property.imageUrl" :alt="property.imageAlt">
+		</div>
 		<div class="p-6">
 			<div class="flex items-baseline">
 				<span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">New</span>
