ce0676aee2e575f6c34e84a0cffed9a4666e75d7
Shaun Mhonde <mhondeshaun@gmail.com>
2019-09-03 12:22:50 +0200

    feat: update navbar links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -29,13 +29,27 @@
 						href="#"
 						class="block mt-4 lg:inline-block lg:mt-0 text-indigo-500 hover:text-indigo-200 mr-4"
 					>
-						Docs
+						Become a host </a
+					><a
+						href="#"
+						class="block mt-4 lg:inline-block lg:mt-0 text-indigo-500 hover:text-indigo-200 mr-4"
+						v-show="loggedIn"
+					>
+						Saved
 					</a>
 					<a
 						href="#"
 						class="block mt-4 lg:inline-block lg:mt-0 text-indigo-500 hover:text-indigo-200 mr-4"
+						v-show="loggedIn"
 					>
-						Examples
+						Trips
+					</a>
+					<a
+						href="#"
+						class="block mt-4 lg:inline-block lg:mt-0 text-indigo-500 hover:text-indigo-200 mr-4"
+						v-show="loggedIn"
+					>
+						Messages
 					</a>
 					<a
 						href="#"
@@ -44,7 +58,7 @@
 						Help
 					</a>
 				</div>
-				<div v-show="showLogin">
+				<div v-show="!loggedIn">
 					<router-link to="/login">
 						<a
 							class="inline-block text-sm px-4 py-2 leading-none border rounded text-indigo-500 border-indigo-500 hover:border-transparent hover:text-white hover:bg-indigo-500 mt-4 lg:mt-0"
@@ -52,7 +66,7 @@
 						>
 					</router-link>
 				</div>
-				<div v-show="!showLogin">
+				<div v-show="loggedIn">
 					<router-link to="/logout">
 						<a
 							class="inline-block text-sm px-4 py-2 leading-none border rounded text-indigo-500 border-indigo-500 hover:border-transparent hover:text-white hover:bg-indigo-500 mt-4 lg:mt-0"
@@ -69,8 +83,8 @@
 export default {
 	props: ['title'],
 	computed: {
-		showLogin() {
-			return !Store.state.isLogged
+		loggedIn() {
+			return Store.state.isLogged
 		},
 	},
 }
