69d50f1c3b6e101d692157f8742513b0279f8395
Shaun Mhonde <mhondeshaun@gmail.com>
2019-09-10 13:51:12 +0200

    feat: added dropdown menu for logged in users

---
+++ src/components/NavDropdown.vue
@@ -0,0 +1,39 @@
+<template>
+	<on-click-outside :do="handleClickOutside">
+		<div class="relative">
+			<div
+				class="tile-img block cursor-pointer leading-tight"
+				@click="open = !open"
+			>
+				<slot name="link"></slot>
+			</div>
+			<div
+				class="absolute right-0 shadow-xl"
+				v-show="open"
+				style="margin-top: 23px"
+			>
+				<slot name="dropdown"></slot>
+			</div>
+		</div>
+	</on-click-outside>
+</template>
+
+<script>
+import OnClickOutside from './OnClickOutside'
+
+export default {
+	components: { OnClickOutside },
+	data() {
+		return {
+			open: false,
+		}
+	},
+	methods: {
+		handleClickOutside() {
+			if (this.open) {
+				this.open = false
+			}
+		},
+	},
+}
+</script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -70,12 +70,55 @@
 					</router-link>
 				</div>
 				<div v-show="loggedIn">
-					<router-link to="/logout">
-						<a
-							class="inline-block text-sm px-4 py-2 leading-none border rounded text-indigo-500 border-indigo-500 hover:border-transparent hover:text-white hover:bg-indigo-500 mt-4 lg:mt-0"
-							>Logout</a
+					<nav-dropdown>
+						<span
+							slot="link"
+							class="text-lg text-right lg:flex-grow text-indigo-500"
 						>
-					</router-link>
+							<span
+								class="block mt-4 lg:inline-block lg:mt-0 hover:text-indigo-200 mr-4"
+							>
+								<img
+									class="w-4 h-4 md:h-8 md:w-8 rounded-full"
+									src="/img/69.png"
+									alt="Avatar"
+								/>
+							</span>
+						</span>
+						<div
+							slot="dropdown"
+							class="bg-white shadow rounded border overflow-hidden"
+						>
+							<router-link to="/profile">
+								<a
+									class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+									>Profile</a
+								>
+							</router-link>
+							<a
+								class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+								>Invite Friends</a
+							>
+							<a
+								class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+								>Refer Hosts</a
+							>
+							<a
+								class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+								>Your Guidebook</a
+							>
+							<a
+								class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+								>Gift Cards</a
+							>
+							<router-link to="/logout">
+								<a
+									class="block px-4 py-3 border-b text-indigo-500 bg-white hover:text-indigo-200 cursor-pointer whitespace-no-wrap"
+									>Logout</a
+								>
+							</router-link>
+						</div>
+					</nav-dropdown>
 				</div>
 			</div>
 		</nav>
@@ -83,7 +126,10 @@
 </template>
 
 <script>
+import NavDropdown from './NavDropdown'
+
 export default {
+	components: { NavDropdown },
 	props: ['title'],
 	computed: {
 		loggedIn() {
