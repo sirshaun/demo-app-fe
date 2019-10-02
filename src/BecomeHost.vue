54804c95eba5c651d51df38bc0ed56a2a6b85dc5
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-02 10:10:32 +0200

    feat: added checkpoint page

--- src/BecomeHost.vue
+++ src/BecomeHost.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="bg-gray-100 h-screen md:mt-22">
-		<div class="mb-10">
+	<div class="bg-gray-100 h-screen" :class="{ 'md:mt-22': !checkpoint }">
+		<div class="mb-10" v-if="!checkpoint">
 			<nav class="nav-float justify-start">
 				<div class="flex items-center flex-shrink-0 text-white mr-6">
 					<router-link to="/">
@@ -14,6 +14,7 @@
 				<div class="font-light text-lg tracking-wide">
 					{{ navText }}
 				</div>
+				<!-- TODO: Add save and exit button -->
 			</nav>
 		</div>
 
@@ -22,13 +23,64 @@
 		<!-- Stepper -->
 		<!-- Place Type, Bedrooms, Baths, Location, Amenities, Shared spaces -->
 
-		<PlaceType v-if="step == 1" />
-		<Bedrooms v-if="step == 2" />
-		<Baths v-if="step == 3" />
-		<Location v-if="step == 4" />
-		<LocationConfirm v-if="step == 5" />
-		<Amenities v-if="step == 6" />
-		<SharedSpaces v-if="step == 7" />
+		<!-- Step One -->
+		<PlaceType
+			v-if="page == 1"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<Bedrooms
+			v-if="page == 2"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<Baths
+			v-if="page == 3"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<Location
+			v-if="page == 4"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<LocationConfirm
+			v-if="page == 5"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<Amenities
+			v-if="page == 6"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+		<SharedSpaces
+			v-if="page == 7"
+			:back="prevPage"
+			:next="nextPage"
+			:checkpoint="toggleCheckpoint"
+		/>
+
+		<!-- Checkpoint -->
+		<Checkpoint
+			:checkpoint="toggleCheckpoint"
+			:step="step"
+			:page="page"
+			@review-step-one="stepOneReview"
+			@continue-to-step-two="proceedStepTwo"
+			@review-step-two="proceedStepTwo"
+			@continue-to-step-three="proceedStepThree"
+			@review-step-three="proceedStepThree"
+			v-if="checkpoint"
+		/>
+
+		<!-- Step Two -->
 	</div>
 </template>
 
@@ -40,6 +92,7 @@ import Location from './components/BecomeHost/Details/Location'
 import LocationConfirm from './components/BecomeHost/Details/LocationConfirm'
 import Amenities from './components/BecomeHost/Details/Amenities'
 import SharedSpaces from './components/BecomeHost/Details/SharedSpaces'
+import Checkpoint from './components/BecomeHost/Checkpoint'
 
 export default {
 	components: {
@@ -50,18 +103,59 @@ export default {
 		LocationConfirm,
 		Amenities,
 		SharedSpaces,
+		Checkpoint,
 	},
 	data() {
 		return {
 			user: {},
+			page: 1,
 			step: 1,
-			text: ['', 'Step 1: Start with the basics'],
+			text: [
+				'Step 1: Start with the basics',
+				'Step 2: Set the scene',
+				'Step 3: Get ready for guests',
+			],
+			checkpoint: false,
 		}
 	},
 	computed: {
 		navText() {
-			// TODO: Update when all steps are known
-			return this.text[this.step]
+			return this.text[this.step - 1]
+		},
+	},
+	methods: {
+		prevPage() {
+			this.page -= 1
+		},
+		nextPage() {
+			this.page += 1
+		},
+		toggleCheckpoint() {
+			this.checkpoint = !this.checkpoint
+		},
+		stepOneReview() {
+			this.step = 1
+			this.page = 1
+		},
+		proceedStepTwo() {
+			this.step = 2
+			this.page = 8
+		},
+		proceedStepThree() {
+			this.step = 2
+			this.page = 8
+		},
+	},
+	watch: {
+		page: {
+			immediate: true,
+			handler: function(page) {
+				if (page > 7 && this.step == 1) {
+					this.toggleCheckpoint()
+				} else if (page > 10 && this.step == 2) {
+					this.toggleCheckpoint()
+				}
+			},
 		},
 	},
 }
