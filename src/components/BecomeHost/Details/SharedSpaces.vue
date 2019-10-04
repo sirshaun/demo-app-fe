558c1538e493cd97789063864b74706d8307bd69
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-04 09:26:47 +0200

    style: added style to shared spaces component

--- src/components/BecomeHost/Details/SharedSpaces.vue
+++ src/components/BecomeHost/Details/SharedSpaces.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="flex flex-col">
-		<h1 class="font-semibold text-2xl text-gray-900">
+		<h1 class="font-semibold text-2xl text-gray-900 px-4">
 			What spaces can guests use?
 		</h1>
-		<p>
+		<p class="px-4">
 			Include common areas, but don’t add spaces that aren’t on your
 			property.
 		</p>
 
-		<div class="flex flex-wrap">
+		<div class="flex flex-wrap mt-6">
 			<div class="w-full px-3">
 				<div class="mb-4" v-for="(option, index) in spaces">
 					<input
@@ -26,35 +26,20 @@
 			</div>
 		</div>
 
-		<hr class="border-gray-300 my-6" />
-
-		<div class="clearfix mt-6">
-			<div class="float-left">
-				<a
-					class="flex items-center text-indigo-600 hover:opacity-50 cursor-pointer"
-					@click="back"
-				>
-					<img
-						src="/img/ikonate/chevron-left-indigo-600.svg"
-						class="h-6 mr-1"
-					/>
-					<span class="font-thin">Back</span>
-				</a>
-			</div>
-			<div class="float-right">
-				<a
-					class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
-					@click="next"
-				>
-					Finish
-				</a>
-			</div>
-		</div>
+		<Footer
+			:back="back"
+			:next="next"
+			:checkpoint="checkpoint"
+			next-btn-text="Done"
+		/>
 	</div>
 </template>
 
 <script>
+import Footer from '@/components/BecomeHost/Footer'
+
 export default {
+	components: { Footer },
 	props: {
 		back: { type: Function, required: true },
 		next: { type: Function, required: true },
