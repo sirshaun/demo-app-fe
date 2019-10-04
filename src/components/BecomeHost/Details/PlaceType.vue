ab13b3a71fb9fb730127c21b316e64e4cebd3dda
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-04 08:30:27 +0200

    refactor: create a become host footer component

--- src/components/BecomeHost/Details/PlaceType.vue
+++ src/components/BecomeHost/Details/PlaceType.vue
@@ -150,45 +150,16 @@
 
 		<CaseModal @close-case-modal="toggleModal" v-if="caseModal" />
 
-		<div
-			class="fixed bg-gray-100 w-2/5 bottom-0 pb-10 z-max"
-			:class="{ 'shadow-top': shadowOn }"
-		>
-			<hr class="border-gray-300 mb-6 -mx-2" />
-
-			<div v-show="false">{{ yScroll }}</div>
-
-			<div class="clearfix">
-				<div class="float-left">
-					<a
-						class="flex items-center text-indigo-600 hover:opacity-50 cursor-pointer"
-						@click="back"
-					>
-						<img
-							src="/img/ikonate/chevron-left-indigo-600.svg"
-							class="h-6 mr-1"
-						/>
-						<span class="font-thin">Back</span>
-					</a>
-				</div>
-				<div class="float-right">
-					<a
-						class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
-						@click="next"
-					>
-						Next
-					</a>
-				</div>
-			</div>
-		</div>
+		<Footer :back="back" :next="next" :checkpoint="checkpoint" />
 	</div>
 </template>
 
 <script>
 import CaseModal from '@/components/BecomeHost/CaseModal'
+import Footer from '@/components/BecomeHost/Footer'
 
 export default {
-	components: { CaseModal },
+	components: { CaseModal, Footer },
 	props: {
 		back: { type: Function, required: true },
 		next: { type: Function, required: true },
@@ -231,8 +202,6 @@ export default {
 				},
 			],
 			space: '',
-			yScroll: 0,
-			shadowOn: false,
 		}
 	},
 	computed: {
@@ -311,20 +280,5 @@ export default {
 			},
 		},
 	},
-	mounted() {
-		const listener = e => this.onScroll()
-
-		window.addEventListener('scroll', listener)
-
-		this.$once('hook:beforeDestroy', () => {
-			overlay.removeEventListener('scroll', listener)
-		})
-	},
 }
 </script>
-
-<style scoped>
-.shadow-top {
-	box-shadow: 0 -10px 5px -10px rgba(0, 0, 0, 0.1);
-}
-</style>
---
+++ src/components/BecomeHost/Footer.vue
@@ -0,0 +1,84 @@
+<template>
+	<div
+		class="fixed bg-gray-100 w-2/5 bottom-0 pb-10 z-max"
+		:class="{ 'shadow-top': shadowOn }"
+	>
+		<hr class="border-gray-300 mb-6 -mx-2" />
+
+		<div v-show="false">{{ yScroll }}</div>
+
+		<div class="clearfix">
+			<div class="float-left">
+				<a
+					class="flex items-center text-indigo-600 hover:opacity-50 cursor-pointer"
+					@click="back"
+				>
+					<img
+						src="/img/ikonate/chevron-left-indigo-600.svg"
+						class="h-6 mr-1"
+					/>
+					<span class="font-thin">Back</span>
+				</a>
+			</div>
+			<div class="float-right">
+				<a
+					class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+					@click="next"
+				>
+					Next
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		back: { type: Function, required: true },
+		next: { type: Function, required: true },
+		checkpoint: { type: Function, required: true },
+	},
+	data() {
+		return {
+			yScroll: 0,
+			shadowOn: false,
+		}
+	},
+	methods: {
+		onScroll() {
+			var supportPageOffset = window.pageXOffset !== undefined
+			var isCSS1Compat = (document.compatMode || '') === 'CSS1Compat'
+
+			this.yScroll = supportPageOffset
+				? window.pageYOffset
+				: isCSS1Compat
+				? document.documentElement.scrollTop
+				: document.body.scrollTop
+		},
+	},
+	watch: {
+		yScroll: {
+			immediate: true,
+			handler: function(yScroll) {
+				this.shadowOn = yScroll == 0
+			},
+		},
+	},
+	mounted() {
+		const listener = e => this.onScroll()
+
+		window.addEventListener('scroll', listener)
+
+		this.$once('hook:beforeDestroy', () => {
+			overlay.removeEventListener('scroll', listener)
+		})
+	},
+}
+</script>
+
+<style scoped>
+.shadow-top {
+	box-shadow: 0 -10px 5px -10px rgba(0, 0, 0, 0.1);
+}
+</style>
