77cf57b93283198c5fa689a5e69d7e9d9f2b1a17
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-07 08:08:02 +0200

    style: added error classes to form fields

--- src/components/BecomeHost/Details/Location.vue
+++ src/components/BecomeHost/Details/Location.vue
@@ -10,7 +10,7 @@
 
 		<div class="flex flex-wrap mt-6 px-4">
 			<a
-				class="bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white p-2 border border-indigo-500 hover:border-transparent rounded flex items-center"
+				class="bg-white hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white p-2 border border-indigo-500 hover:border-transparent rounded flex items-center"
 			>
 				<svg
 					class="h-5 fill-current"
@@ -34,6 +34,9 @@
 				<div class="relative">
 					<select
 						class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+						:class="{
+							'border-red-500 bg-red-100 mb-1': countryError,
+						}"
 						id="grid-country"
 						v-model="country"
 					>
@@ -58,31 +61,40 @@
 						</svg>
 					</div>
 				</div>
+				<p v-show="countryError" class="text-red-500 text-xs italic">
+					Please select an option
+				</p>
 			</div>
 		</div>
 
 		<div class="flex flex-wrap mt-6">
 			<div class="w-full px-3">
 				<label
-					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					class="block text-gray-600 tracking-wide text-light mb-2"
 					for="grid-street"
 				>
 					Street Address
 				</label>
 				<input
 					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+					:class="{
+						'border-red-500 bg-red-100 mb-1': streetError,
+					}"
 					id="grid-street"
 					type="text"
 					placeholder="e.g. 123 Main St"
 					v-model="street"
 				/>
+				<p v-show="streetError" class="text-red-500 text-xs italic">
+					Please enter street number
+				</p>
 			</div>
 		</div>
 
 		<div class="flex flex-wrap mt-6">
 			<div class="w-full px-3">
 				<label
-					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					class="block text-gray-600 tracking-wide text-light mb-2"
 					for="grid-apt-num"
 				>
 					Apt, Suite. (optional)
@@ -100,55 +112,73 @@
 		<div class="flex flex-wrap mt-6">
 			<div class="w-1/2 px-3">
 				<label
-					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					class="block text-gray-600 tracking-wide text-light mb-2"
 					for="grid-city"
 				>
 					City
 				</label>
 				<input
 					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+					:class="{
+						'border-red-500 bg-red-100 mb-1': cityError,
+					}"
 					id="grid-city"
 					type="text"
 					placeholder="e.g. Surrey Hills"
 					v-model="city"
 				/>
+				<p v-show="cityError" class="text-red-500 text-xs italic">
+					Please enter city
+				</p>
 			</div>
 			<div class="w-1/2 px-3">
 				<label
-					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					class="block text-gray-600 tracking-wide text-light mb-2"
 					for="grid-state"
 				>
 					State
 				</label>
 				<input
 					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+					:class="{
+						'border-red-500 bg-red-100 mb-1': stateError,
+					}"
 					id="grid-state"
 					type="text"
 					placeholder="e.g. NSW"
 					v-model="state"
 				/>
+				<p v-show="stateError" class="text-red-500 text-xs italic">
+					Please enter state
+				</p>
 			</div>
 		</div>
 
 		<div class="flex flex-wrap mt-6">
 			<div class="w-1/2 px-3">
 				<label
-					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					class="block text-gray-600 tracking-wide text-light mb-2"
 					for="grid-postcode"
 				>
 					Zip code
 				</label>
 				<input
 					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+					:class="{
+						'border-red-500 bg-red-100 mb-1': postcodeError,
+					}"
 					id="grid-postcode"
 					type="text"
 					placeholder="e.g. 2010"
 					v-model="postcode"
 				/>
+				<p v-show="postcodeError" class="text-red-500 text-xs italic">
+					Please enter postcode / zip code
+				</p>
 			</div>
 		</div>
 
-		<Footer :back="back" :next="next" :checkpoint="checkpoint" />
+		<Footer :back="back" :next="proceed" :checkpoint="checkpoint" />
 	</div>
 </template>
 
@@ -171,9 +201,52 @@ export default {
 			city: '',
 			state: '',
 			postcode: '',
+			countryError: false,
+			streetError: false,
+			cityError: false,
+			stateError: false,
+			postcodeError: false,
 		}
 	},
+	computed: {
+		errors() {
+			return (
+				this.countryError ||
+				this.streetError ||
+				this.cityError ||
+				this.stateError ||
+				this.postcodeError
+			)
+		},
+	},
 	methods: {
+		checkCountry() {
+			this.countryError = this.country == ''
+		},
+		checkStreet() {
+			this.streetError = this.street == ''
+		},
+		checkAptNum() {
+			this.aptNumError = this.aptNum == ''
+		},
+		checkCity() {
+			this.cityError = this.city == ''
+		},
+		checkState() {
+			this.stateError = this.state == ''
+		},
+		checkPostcode() {
+			this.postcodeError = this.postcode == ''
+		},
+		proceed() {
+			this.checkCountry() ||
+				this.checkStreet() ||
+				this.checkCity() ||
+				this.checkState() ||
+				this.checkPostcode()
+
+			if (!this.errors) this.next()
+		},
 		fetchCountries() {
 			this.$http.get('/location/countries').then(
 				res => {
