082cf9a1bf6db2069766777b8fe35a0d4df4b0af
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-10 12:20:55 +0200

    fix: return truthy or falsey

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,14 @@
 export const getters = {
-  progress: state => {
-    return !state.listing.progress;
-  },
-  listing_id: state => {
-    return state.listing.id;
-  }
-};
+	progress: state => {
+		return !state.listing.progress
+	},
+	listing_id: state => {
+		return state.listing.id
+	},
+	photosUploaded: state => {
+		return !!state.listing.photos.length
+	},
+	title: state => {
+		return state.listing.title
+	},
+}
