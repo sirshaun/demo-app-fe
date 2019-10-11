e700f7abd632865baa4e39bebbdfd0e2a623ff32
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-11 09:40:28 +0200

    feat: added extra details components

---
+++ src/components/BecomeHost/AddExtraDetail.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="flex flex-wrap my-3">
+    <div class="w-full flex flex-col">
+      <div class="flex items-center">
+        <input
+          type="checkbox"
+          :id="'extra-detail-' + index"
+          :name="'extra-detail-' + index"
+          @click="toggleDescriptionField"
+        />
+        <label
+          :for="'extra-detail-' + index"
+          class="ml-2 font-light inline-block w-11/12"
+          >{{ option.text }}</label
+        >
+      </div>
+      <div v-if="descriptionFieldOn">
+        <div class="text-gray-600">
+          {{ option.descr }}
+        </div>
+        <input
+          class="appearance-none block w-4/5 h-12 bg-gray-200 text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+          id="description"
+          type="text"
+          placeholder="Add your description"
+          v-model="description"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: { type: Number, required: true },
+    option: { type: Object, required: true },
+    addDetail: { type: Function, required: true },
+    removeDetail: { type: Function, required: true },
+    updateDetailDescription: { type: Function, required: true },
+  },
+  data() {
+    return {
+      descriptionFieldOn: false,
+      description: '',
+    }
+  },
+  methods: {
+    toggleDescriptionField() {
+      if (this.descriptionFieldOn) {
+        this.removeDetail(this.index)
+      } else {
+        this.collateData()
+      }
+
+      this.descriptionFieldOn = !this.descriptionFieldOn
+    },
+    collateData() {
+      var detail = { detail: this.option.text, description: this.description }
+
+      this.addDetail(this.index, detail)
+    },
+    sluggedName(name) {
+      return name
+        .toLowerCase()
+        .replace(/[^a-zA-Z ]/g, ' ')
+        .replace(' ', '_')
+    },
+  },
+  watch: {
+    description: {
+      immediate: false,
+      handler: function(description) {
+        this.updateDetailDescription(this.index, this.description)
+      },
+    },
+  },
+}
+</script>
--- src/components/BecomeHost/ExtraDetails.vue
+++ src/components/BecomeHost/ExtraDetails.vue
@@ -1,9 +1,87 @@
 <template>
-  <div></div>
+  <div class="mt-6 mb-20">
+    <div class="block tracking-wide text-gray-700 font-bold mb-4">
+      Details guests must know about your home
+    </div>
+
+    <AddExtraDetail
+      v-for="(option, index) in options"
+      :option="option"
+      :index="index"
+      :key="index"
+      :add-detail="addDetail"
+      :remove-detail="removeDetail"
+      :update-detail-description="updateDetailDescription"
+    />
+  </div>
 </template>
 
 <script>
+import AddExtraDetail from './AddExtraDetail'
+
 export default {
+  components: { AddExtraDetail },
   props: {},
+  data() {
+    return {
+      extraDetails: [],
+      options: [
+        {
+          text: 'Must climb stairs',
+          descr: 'Describe the stairs (for example, how many flights)',
+        },
+        {
+          text: 'Potential for noise',
+          descr: 'Describe the noise and when it’s likely to take place',
+        },
+        {
+          text: 'Pet(s) live on property',
+          descr: 'Describe the pets you have',
+        },
+        {
+          text: 'No parking on property',
+          descr: 'Describe the parking situation around your listing',
+        },
+        {
+          text: 'Some spaces are shared',
+          descr: 'Describe the spaces the guests will share',
+        },
+        {
+          text: 'Amenity limitations',
+          descr:
+            'Describe an amenity or service that’s limited, such as weak Wi-Fi or limited hot water',
+        },
+        {
+          text: 'Surveillance or recording devices on property',
+          descr:
+            'Describe any device that records or sends video, audio, or still images. Specify each device’s location and whether it will be on or off. Learn more',
+          link: '',
+        },
+        {
+          text: 'Weapons on property',
+          descr:
+            'Describe any weapons (firearms, airguns, tasers, etc.), their location, and how they’re secured Learn more',
+          link: '',
+        },
+        {
+          text: 'Dangerous animals on property',
+          descr:
+            'Describe any domesticated or wild animals that could be a health or safety risk to guests or other animals Learn more',
+          link: '',
+        },
+      ],
+    }
+  },
+  methods: {
+    addDetail(index, detail) {
+      this.extraDetails[index] = detail
+    },
+    removeDetail(index) {
+      this.extraDetails.splice(index, 1)
+    },
+    updateDetailDescription(index, description) {
+      this.extraDetails[index].description = description
+    },
+  },
 }
 </script>
