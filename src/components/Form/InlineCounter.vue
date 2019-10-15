18daa3325304aff85d0b763300cf168ed994899a
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-15 11:59:08 +0200

    fix: counter functionality, and upward feedback

--- src/components/Form/InlineCounter.vue
+++ src/components/Form/InlineCounter.vue
@@ -3,17 +3,24 @@
     <div class="flex justify-between">
       <div class="flex items-center" @click="getFocus">
         <input
-          class="w-2 flex-grow appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-          type="text"
-          id="grid-counter"
+          class="ml-2 w-5 flex-grow appearance-none bg-transparent border-none text-gray-700 text-lg leading-tight focus:outline-none"
+          type="number"
+          :id="'grid-counter-' + id"
+          v-model="count"
         />
-        <span class="font-light text-gray-600">{{ text }}</span>
+        <span class="ml-1 font-light text-gray-600">{{ text }}</span>
       </div>
       <div class="flex items-center">
-        <div class="border-l border-gray-500 px-4">
+        <div
+          class="border-l border-gray-500 px-4 cursor-pointer"
+          @click="decrement"
+        >
           <img class="h-8" src="/img/ikonate/minus-gray-600.svg" />
         </div>
-        <div class="border-l border-gray-500 pl-4 pr-2">
+        <div
+          class="border-l border-gray-500 pl-4 pr-2 cursor-pointer"
+          @click="increment"
+        >
           <img class="h-8" src="/img/ikonate/plus-gray-600.svg" />
         </div>
       </div>
@@ -25,12 +32,51 @@
 export default {
   props: {
     text: { type: String, required: true },
+    id: { type: String, required: true },
     func: { type: Function, required: true },
+    initialCount: { type: Number, default: 0 },
+  },
+  data() {
+    return {
+      count: this.initialCount,
+    }
   },
   methods: {
+    increment() {
+      if (this.count == '') this.count = 0
+
+      this.count += 1
+    },
+    decrement() {
+      if (this.count == '') this.count = 0
+
+      if (this.count > 0) this.count -= 1
+    },
     getFocus() {
-      document.getElementById('grid-counter').focus()
+      document.getElementById('grid-counter-' + this.id).focus()
+    },
+  },
+  watch: {
+    count: {
+      immediate: true,
+      handler: function(count) {
+        if (count == 0) this.count = ''
+        this.func(count)
+      },
     },
   },
 }
 </script>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  /* display: none; <- Crashes Chrome on hover */
+  -webkit-appearance: none;
+  margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
+
+input[type='number'] {
+  -moz-appearance: textfield; /* Firefox */
+}
+</style>
