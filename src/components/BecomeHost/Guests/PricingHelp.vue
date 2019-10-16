1fc7a45000eea9a741969c0e0d2d5f5c9df62d3e
Shaun Mhonde <mhondeshaun@gmail.com>
2019-10-16 10:41:08 +0200

    feat: pricing helper slider (wip)

---
+++ src/components/BecomeHost/Guests/PricingHelp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-32 px-4 bg-pink-100 h-200">
+    <span class="font-light text-lg text-gray-700">content goes here ...</span>
+    <!-- <Glider>
+      <ul class="glide__slides">
+        <li class="glide__slide max-w-sm"></li>
+      </ul>
+    </Glider> -->
+  </div>
+</template>
+
+<script>
+import Glider from '@/components/Glider'
+
+export default {
+  components: { Glider },
+}
+</script>
